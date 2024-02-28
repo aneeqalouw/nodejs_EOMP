63a415a083064dc908598c9e5ebc77d4b670ad21
aneeqalouw <aneeqalouw057@gmail.com>
2024-02-28 08:42:32 +0200

    o

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,8 +1,8 @@
 <template>
-<div id="about" class="d-flex justify-content-center" style="margin-inline: 130px;">
-  <div class="row d-flex justify-content-center">
-    <div class="col">
-      <h2 class="fs-1">About us</h2>
+<div class="vh-100" style="margin-top: 50px;">
+  <div class="container" id="about">
+    <br><br>
+    <h2 class="fs-1">About us</h2>
     <p>
       Welcome to Altitude Apparel, where we elevate your aerial dance experience with our exceptional and high grade clothing line. Founded by passionate aerialist Aneeqa and managed by Head strong CEO Inganathi, we understand the importance of quality, comfort, and style in your aerial dance journey.
       <br><br>
@@ -10,11 +10,10 @@
       <br><br>
       At AA, we pride ourselves on being more than just an online clothing store - we're a passionate community of aerialists dedicated to enhancing your aerial dance experience and making sure you practice safely and comfortablely.
       Our journey began with a vision to create clothing that not only meets the rigorous demands of aerial routines but is also exclusive as in that you cannot find garments like this anywhere else in South Africa. From the first sketch to the final stitch, each garment in our collection is meticulously crafted with the highest standards of quality, functionality, and style in mind. We believe that every aerialist deserves apparel that not only supports their movements but also celebrates their individuality and creativity.
-    </p> <br> <br>
-    </div>
-    <div class="col d-none d-lg-flex">
-      <img src="https://i.ibb.co/ncFMQVR/female-blond-aerial-performer-with-white-aerial-silks-isolated-black-background-403777-439.jpg" alt="Aerialist" loading="lazy" style="width: 30rem; height: 39rem;">
-    </div>
+      <br><br>
+      What sets us apart is our unwavering commitment to inclusivity, sustainability, and community. We celebrate the diversity of the aerial community by offering a wide range of sizes and styles that cater to all body types and preferences. Our dedication to sustainability extends beyond just using eco-friendly materials - we're committed to minimizing waste, reducing our carbon footprint, and supporting ethical manufacturing practices every step of the way. Altitude Apparel is here to be your trusted partner in your aerial journey. Join us as we soar to new heights together, one spin, drop, and climb at a time.
+    </p>
+    <br><br>
   </div>
 </div>
 </template>

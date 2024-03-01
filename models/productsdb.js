fd3962d931eca784912e7fcfffd4bc2ef1a34e55
InganathiStemela <inganathistemela13@gmail.com>
2024-03-01 11:24:22 +0200

    added

--- models/productsdb.js
+++ models/productsdb.js
@@ -28,22 +28,20 @@ class Products{
         })
     })
     }
-    addProduct(req, res) {
-        const data = req.body
-        const query = `
-        INSERT INTO products
-        SET ?;
+
+    addProduct(req, res){
+        const qry = `
+            INSERT INTO products
+            SET ?;
         `
-        db.query(query,[data], (err) => {
-            if(err) {
-                throw err
-            } else {
-                res.json({ 
-                    status: res.statusCode, 
-                    msg: "Product added!" 
-                })
-            }
+        db.query(qry, [req.body], (err)=>{
+            if(err) throw err
+            res.json({
+                status: res.statusCode,
+                msg: 'Product successfully added'
+            })
         })
+
     }
     updateProduct(req, res){
         const qry = `
